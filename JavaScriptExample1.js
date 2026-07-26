0bd6ce471aed0c2518f98d7bfc14c926108fbe2e
vattamwar-shankar <vattamwarshankar@gmail.com>
2026-07-26 12:37:47 +0530

    Learning chapter1

---
+++ JavaScriptExample1.js
@@ -0,0 +1,11 @@
+console.log("Hello World!");
+console.table(["Apple", "Mango"]);
+console.error("Invalid Input");
+
+
+
+
+//for running this code, you need to have Node.js installed on your system. 
+// You can run this JavaScript file using the command line by navigating 
+// to the directory where the file is located 
+// and executing the command `node JavaScriptExample1.js`. 
